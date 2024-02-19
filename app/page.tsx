13ebddb5d00979503203fe6906b8ea09b1b33c2d
Shikhar Singh <shikharsingh8969@gmail.com>
2024-02-19 18:11:25 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,143 @@
 import Image from "next/image";
+import Typed from "typed.js";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header>
+        <nav>
+          <div className="left">Shikhar's Portfolio</div>
+          <div className="right">
+            <ul>
+              <li>
+                <a href="#Home">Home</a>
+              </li>
+              <li>
+                <a href="#About">About</a>
+              </li>
+              <li>
+                <a href="/">Services</a>
+              </li>
+              <li>
+                <a href="#Projects">Projects</a>
+              </li>
+              <li>
+                <a href="/">Contact Me</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section className="firstSection">
+          <div className="leftSection">
+            <div>
+              Hi, My name is<span className="purple"> Shikhar</span>
+            </div>
+            <br />I am Web Developer and Programmer
+            <div className="buttons">
+              <a href=""></a>
+              <a href="ShikharSingh_resume.pdf" download>
+                <button className="btn">Download Resume</button>
+              </a>
+              <a href="https://github.com/shikhar2712" target="_blank">
+                <button className="btn">Github</button>
+              </a>
+            </div>
+          </div>
+
+          <div className="rightSection">
+            <img src="/PngItem_1127587.png" alt="image" />
+          </div>
+        </section>
+        <hr />
+        <section className="secondsection">
+          <span className="text-gray">What I have done so far</span>
+          <div id="Projects">
+            <h2>Work Experience</h2>
+          </div>
+
+          <div className="box">
+            <div className="vertical">
+              <div className="date">Jul'23-Sept'23</div>
+              <div className="vertical-title">SDE Intern</div>
+              <div className="vertical-desc">
+                APIs Integration.
+                <div>Code Optimisation.</div>
+                <div>APIs testing.</div>
+              </div>
+            </div>
+            <div className="vertical">
+              <div className="image-top">
+                <Image
+                  src="/Python-logo-notext.svg.png"
+                  width={100}
+                  height={100}
+                  alt="Pythonlogo"
+                />
+              </div>
+              <div className="vertical-title1">Web Scraper</div>
+              <div className="vertical-desc1">
+                Developed a Python-based web scraper to extract data from
+                GitHub-topics, specifically focusing on top repositories.
+              </div>
+            </div>
+            <div className="vertical">
+              <div className="image-top1">
+                <Image
+                  src="/image-removebg-preview.png"
+                  width={54}
+                  height={100}
+                  alt="image"
+                />
+              </div>
+              <div className="vertical-title2">Movie-info-Extractor</div>
+              <div className="vertical-desc2">
+                <div>Flask API with API key authentication.</div>
+                <div>Retrieves Movie details.</div>
+                <div>Open-Source Integration.</div>
+              </div>
+            </div>
+            <div className="vertical">
+              <div className="image-top">
+                <Image
+                  src="/pngwing.com.png"
+                  width={50}
+                  height={38}
+                  alt="image"
+                />
+              </div>
+              <div className="vertical-title3">Portfolio Website</div>
+              <div className="vertical-desc3">
+                Used HTML and CSS to make this site.
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <div className="footer">
+          <div className="footer-first">
+            <h3 className="summary">Shikhar's Portfolio Summary</h3>
+            <div id="About">
+              <div className="summary-text">
+                Motivated and dedicated student , I'm passionate about pursuing
+                a career as a Software Developer . Possessing 3 years of
+                Programming experience and expertise in OOP , alongside
+                proficiency in C, C++, Python (including Flask), API
+                development, HTML, CSS, JS and a strong foundation in data
+                structures and algorithms , I'm well-equipped to contribute. My
+                academic excellence, practical experience through internships
+                and projects, and commitment to continuous learning drive my
+                ambitions. I'm enthusiastic about securing an SDE internships
+                and job role to apply my skills, drive, and problem-solving
+                abilities to innovate in a dynamic team.
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </footer>
+    </>
   );
 }
